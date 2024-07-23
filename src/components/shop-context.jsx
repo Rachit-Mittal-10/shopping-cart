2237e393fddc38b13a910ec48980095c7ea89c6d
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-07-24 02:17:43 +0530

    Shop Page completed

---
+++ src/components/shop-context.jsx
@@ -0,0 +1,29 @@
+import React, { createContext, useState } from 'react';
+import {PRODUCTS} from '../product';
+
+// This will provide the everything related to state management
+
+export const ShopContext = createContext(null);
+
+const getDefaultCart = () => {
+    let cart = {}
+    for(let i=1;i<=PRODUCTS.length;i++){
+        cart[i] = 0;
+    }
+    return cart;
+}
+
+export const ShopContextProvider = (props) => {
+    const [cartItems, setCartItems] = useState(getDefaultCart());
+    const addToCart = (itemID) => {
+        setCartItems((prev) => ({...prev, [itemID]:prev[itemID]+1}));
+    }
+    const removeFromCart = (itemID) => {
+        setCartItems((prev) => ({...prev, [itemID]:prev[itemID]-1}));
+    }
+    const contextValue = {cartItems, addToCart,removeFromCart};
+    console.log(cartItems);
+    return (
+        <ShopContext.Provider value={contextValue}>{props.children}</ShopContext.Provider>
+    );
+}
--- src/pages/shop/components/product.jsx
+++ src/pages/shop/components/product.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import './product.css';
+import { ShopContext } from '../../../components/shop-context';
 
 export const Product = (props) =>{
     const {productID, productName, price, productImage} = props.data;
+    const {cartItems,addToCart} = useContext(ShopContext);
+    let cartItemsAmount = cartItems[productID];
     return (
-        <div className="product">
+        <div className="product" key={productID}>
             <img src={productImage} alt={productName+"'s Image"}/>
             <div className="product-description">
                 <p>{productName}</p>
                 <p>Price: {price}</p>
             </div>
+            <button onClick={() => addToCart(productID)}>Add To Cart {cartItemsAmount>0 && ("(" + cartItemsAmount + ")")}</button>
         </div>
     );
 }
