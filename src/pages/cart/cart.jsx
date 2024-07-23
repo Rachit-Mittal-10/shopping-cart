b7853c700498d06efc75eb5875a68e022b9415ea
Rachit Mittal <mittalrachit10063003@gmail.com>
2024-07-24 02:26:48 +0530

    updated fileStructure

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import {PRODUCTS} from '../../product';
+import {Product} from '../components/product';
 
 export const Cart = () => {
     return (
-        <div>Cart</div>
+        <div classNaame="cart">
+            <div className='titles'>
+                <h1>Your Cart</h1>
+            </div>
+            <div className="cart-items">
+                {
+                    PRODUCTS.map(()=>{
+
+                    })
+                }
+            </div>
+        </div>
     );
 }
